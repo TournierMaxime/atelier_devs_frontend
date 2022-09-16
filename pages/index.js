fa657278e4524d5d9c39d34ca7481811f3e33b97
Hoggy06 <speedrun.fra@gmail.com>
2022-09-16 21:23:51 +0200

    Page du profil

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { loginContext } from "../components/Context/context";
 import Login from "../components/Auth/Login";
+import Profil from "../components/Profil/Profil";
 export default function Home() {
   //Variables
-  const { isLogged, setIsLogged } = useContext(loginContext);
-  return isLogged ? null : <Login />;
+  const { isLogged } = useContext(loginContext);
+  return isLogged ? <Profil /> : <Login />;
 }
