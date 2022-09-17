db5099ce541e8b86f5e3bfcef9f544a602240077
Hoggy06 <speedrun.fra@gmail.com>
2022-09-17 11:10:34 +0200

    redirection vers la page profil

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { loginContext } from "../components/Context/context";
 import Login from "../components/Auth/Login";
 import Profil from "../components/Profil/Profil";
+import { useRouter } from "next/router";
 export default function Home() {
+  const router = useRouter();
   //Variables
-  const { isLogged } = useContext(loginContext);
-  return isLogged ? <Profil /> : <Login />;
+  const { isLogged, userId } = useContext(loginContext);
+  return isLogged ? (
+    <Fragment>
+      <Profil id={userId} />
+    </Fragment>
+  ) : (
+    <Login />
+  );
 }
