1bfe5306deec3eff5d94c751582891b9446fc7a6
Hoggy06 <speedrun.fra@gmail.com>
2022-09-20 14:54:05 +0200

    Ajout de la création de commentaire

---
+++ components/Info/Comments/Create.js
@@ -0,0 +1,117 @@
+//Imports
+import { useState, Fragment, useContext } from "react";
+import { Button } from "primereact/button";
+import { Message } from "primereact/message";
+import { Divider } from "primereact/divider";
+import { Editor } from "primereact/editor";
+import { Card } from "primereact/card";
+import { Dialog } from "primereact/dialog";
+import { headerTemplate } from "../../Functions/Toolbar";
+import axios from "axios";
+import { loginContext } from "../../Context/context";
+export default function Create({ setData, postId }) {
+  //Variables
+  const { isLogged, token } = useContext(loginContext);
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
+  const [createDialog, setCreateDialog] = useState(false);
+  const toggleCreate = () => {
+    setCreateDialog(!createDialog);
+  };
+  //Retrieve data comments
+  async function getData() {
+    try {
+      const request = await fetch(
+        `${process.env.URL_BACKEND}/api/posts/${postId}/comments?page=1`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const response = request;
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //Form for create a comment
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = { message };
+
+    axios({
+      method: "post",
+      url: `${process.env.URL_BACKEND}/api/posts/${postId}/comment/new`,
+      data: formData,
+      headers: {
+        Authorization: `Bearer ` + token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.error) {
+          setError(res.error);
+        }
+        setSuccess(res.data.message);
+        setData(formData);
+        getData();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setMessage("");
+    setSuccess("");
+    setError("");
+  };
+  return (
+    <Fragment>
+      {isLogged ? (
+        <Fragment>
+          <Divider />
+          <div className="m-auto">
+            <Button onClick={toggleCreate}>Poster un commentaire</Button>
+          </div>
+          {createDialog ? (
+            <Dialog
+              className="dialog"
+              header="Poster un commentaire"
+              modal
+              closeOnEscape
+              draggable={false}
+              onHide={toggleCreate}
+              visible={createDialog}
+            >
+              <Card className="card flex justify-content-center w-auto m-auto xl:col-8 col-offset-2 lg:col-8 col-offset-2 md:col-8 col-offset-2 sm:col-10 col-offset-1">
+                <form onSubmit={onSubmit} className="p-fluid">
+                  <div className="field">
+                    <label>Commentaire</label>
+                    <Editor
+                      value={message}
+                      name="message"
+                      type="text"
+                      className="editor"
+                      onTextChange={(e) => setMessage(e.htmlValue)}
+                      headerTemplate={headerTemplate}
+                      style={{ height: "320px" }}
+                    />
+                    <Divider />
+                  </div>
+                  <Button type="submit" label="Commenter" className="mt-2" />
+                  <Divider />
+                  {error ? <Message severity="error" text={error} /> : null}
+                  {success ? (
+                    <Message severity="success" text={success} />
+                  ) : null}
+                </form>
+              </Card>
+            </Dialog>
+          ) : null}
+        </Fragment>
+      ) : null}
+    </Fragment>
+  );
+}
